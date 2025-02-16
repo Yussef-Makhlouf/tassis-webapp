f2e640ad31dedfb2b4421fcff26e998cf845d86d
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-02-16 08:21:45 +0200

    pre finsh

--- next.config.mjs
+++ next.config.mjs
@@ -5,33 +5,29 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
   images: {
     domains: ['ik.imagekit.io'],
-    unoptimized: process.env.NODE_ENV === 'production',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  output: 'standalone',
   poweredByHeader: false,
   compress: true,
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-    turbo: {
-      loaders: {
-        '.svg': ['@svgr/webpack'],
-      },
-    },
-  },
-  webpack: (config) => {
-    config.optimization.moduleIds = 'deterministic';
-    return config;
-  },
 };
 
-// Analyze bundle if ANALYZE is set
 const analyzeBundleConfig = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })(nextConfig);
 
-// Export the final config with all enhancements
 export default withNextIntl(analyzeBundleConfig);
