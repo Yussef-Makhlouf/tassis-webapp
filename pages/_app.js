ba6919216743540d66f1c49427b924aa8cc941c1
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-02-19 11:45:54 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,26 @@ function MyApp({ Component, pageProps }) {
         }}
       />
 
+      {/* Google Tag Manager (noscript fallback) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5F9XMBCV"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+
+      {/* Google Tag Manager */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5F9XMBCV');
+          `,
+        }}
+      />
+
       {/* Rest of the application */}
       <Component {...pageProps} />
     </>
