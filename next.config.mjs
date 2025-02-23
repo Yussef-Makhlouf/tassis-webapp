148e5c886ee656517df07e6df87f558e7b16224d
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-02-23 18:29:31 +0200

    add video

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,19 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/i,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      }],
+    });
+    return config;
+  },
   poweredByHeader: false,
   compress: true,
   reactStrictMode: true,
