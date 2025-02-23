c27dd92c004cf8a7f22ad8885e801ba1a4b9b0ba
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-02-23 19:04:42 +0200

    add video

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -214,7 +214,7 @@ export default function Footer() {
 
           <div className="w-full rounded-2xl overflow-hidden shadow-lg border-2 border-[#20284D]">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3624.7960960420257!2d46.6893654!3d24.7136225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03890d489399%3A0xba974d1c98e79fd5!2z2KfZhNix2YrYp9i2INin2YTYs9i52YjYr9mK2Kk!5e0!3m2!1sar!2ssa!4v1709917391044!5m2!1sar!2ssa"
+              src="https://maps.app.goo.gl/uGZiRNRoWJUdbKqu6?g_st=com.google.maps.preview.copy"
               width="100%"
               height="100%"
               style={{ border: 0 }}
